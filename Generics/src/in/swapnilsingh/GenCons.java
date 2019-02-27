6359eaf4ccc588cc6cc9a45a3f208880ce54b440
swapnil singh <swappp7singh@gmail.com>
2019-02-27 17:55:33 +0530

    Included everything till generics constructor module

---
+++ Generics/src/in/swapnilsingh/GenCons.java
@@ -0,0 +1,28 @@
+package in.swapnilsingh;
+
+// Use a generic constructor
+class GenCons {
+
+    private double val;
+
+    <T extends Number> GenCons(T args) {
+        val = args.doubleValue();
+    }
+
+    void showVal() {
+        System.out.println("val: " + val);
+    }
+
+}
+
+class GenConsDemo {
+    public static void main(String[] args) {
+
+        GenCons test = new GenCons(100);
+        GenCons test2 = new GenCons(123.5F);
+
+        test.showVal();
+        test2.showVal();
+
+    }
+}
